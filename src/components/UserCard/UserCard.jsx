d9c28aba7f3234b0c77b71aee9a2ab803cba093a
Vlad <developertitarenko@gmail.com>
2021-07-08 18:47:31 +0300

    update userCard

--- src/components/UserCard/UserCard.jsx
+++ src/components/UserCard/UserCard.jsx
@@ -1,7 +1,8 @@
 import './userCard.scss'
+import avatar from '../../static/png-transparent-avatar-user-computer-icons-software-developer-avatar-child-face-heroes.png'
 const UserCard = ({firstName, lastName, username, position, logo, index}) => (
   <div className='user-card' key={index}>
-    <img src={logo} alt=""/>
+    <img src={logo ? logo : avatar} alt=""/>
     <div className={'user-card__information'}>
       <span>first name: {firstName}</span>
       <span>last name: {lastName}</span>
